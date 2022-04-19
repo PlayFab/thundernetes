a7f95c376929ce18ac5914588d60b8ce8cc84d76
Javier Morales <javiermoralesr95@gmail.com>
2022-04-19 09:56:21 -0400

    Added prometheus metrics endpoint to qos server.

--- cmd/qosserver/main.go
+++ cmd/qosserver/main.go
@@ -1,17 +1,49 @@
 package main
 
 import (
+	"fmt"
 	"net"
+	"net/http"
+	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	log "github.com/sirupsen/logrus"
 )
 
+var QoSServerRequests = promauto.NewCounterVec(prometheus.CounterOpts{
+	Namespace: "thundernetes",
+	Name:      "qos_server_requests",
+	Help:      "Number of requests made to the QoS server",
+}, []string{"remote_ip"})
+
 func main() {
-	addr, err := net.ResolveUDPAddr("udp", ":3075")
+	go metricsServer(3075)
+	qosServer(3075)
+}
+
+func metricsServer(port int) {
+	http.Handle("/metrics", promhttp.Handler())
+	srv := &http.Server{
+		ReadTimeout:    5 * time.Second,
+		WriteTimeout:   5 * time.Second,
+		MaxHeaderBytes: http.DefaultMaxHeaderBytes,
+		Addr:           fmt.Sprintf(":%d", port),
+	}
+	log.Infof("Starting metrics server on port %s", srv.Addr)
+	err := srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Infof("Starting UDP server on port %d", addr.Port)
+}
+
+func qosServer(port int) {
+	addr, err := net.ResolveUDPAddr("udp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Infof("Starting UDP server on port :%d", addr.Port)
 	conn, err := net.ListenUDP("udp", addr)
 	if err != nil {
 		log.Fatal(err)
@@ -34,6 +66,7 @@ func main() {
 				continue
 			}
 			log.Infof("Valid input, sent %d bytes response to remote address %v", count, remote)
+			QoSServerRequests.WithLabelValues(remote.IP.String()).Inc()
 		}
 	}
 }
