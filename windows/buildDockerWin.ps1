8dd10f6c12a1956b9d5fcff28c24a4dd3bd6ba2a
Javier Morales <javiermoralesr95@gmail.com>
2022-05-09 17:51:10 -0400

    Updated script for building windows images

--- windows/buildDockerWin.ps1
+++ windows/buildDockerWin.ps1
@@ -1,17 +1,20 @@
-param ($repo)
-$imageVersion = git rev-list HEAD --max-count=1 --abbrev-commit
-$imageVersion = ":"+$imageVersion
+param ($repo, $imageVersion)
+if ($imageVersion -eq "")
+{
+    $version = git rev-list HEAD --max-count=1 --abbrev-commit
+}
+$version = ":"+$version
 $dockerfilesWin = Get-ChildItem -Include Dockerfile.win -Recurse
 $dockerFolders = Split-Path -parent $dockerfilesWin
 for($i=0; $i -lt $dockerfilesWin.Length; $i++)
 {
     $imageName = [System.IO.Path]::GetFileNameWithoutExtension($dockerFolders[$i])
     $imageName = "thundernetes-"+$imageName+"-win"
-    docker build -t $repo$imageName$imageVersion -f $dockerfilesWin[$i] .
+    docker build -t $repo$imageName$version -f $dockerfilesWin[$i] .
 }
 for($i=0; $i -lt $dockerfilesWin.Length; $i++)
 {
     $imageName = [System.IO.Path]::GetFileNameWithoutExtension($dockerFolders[$i])
     $imageName = "thundernetes-"+$imageName+"-win"
-    docker push $repo$imageName$imageVersion
+    docker push $repo$imageName$version
 }
