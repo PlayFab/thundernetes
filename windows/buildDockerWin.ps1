c3a9aa6d55eb59489e2b7820cee58e9d4ef308cb
Javier Morales <javiermoralesr95@gmail.com>
2022-05-10 12:47:29 -0400

    Fixed var name in powershell script

--- windows/buildDockerWin.ps1
+++ windows/buildDockerWin.ps1
@@ -17,11 +17,11 @@ for($i=0; $i -lt $dockerfilesWin.Length; $i++)
 {
     $imageName = [System.IO.Path]::GetFileNameWithoutExtension($dockerFolders[$i])
     $imageName = "thundernetes-"+$imageName+"-win"
-    docker build -t $repo$imageName$version -f $dockerfilesWin[$i] .
+    docker build -t $registry$imageName$version -f $dockerfilesWin[$i] .
 }
 for($i=0; $i -lt $dockerfilesWin.Length; $i++)
 {
     $imageName = [System.IO.Path]::GetFileNameWithoutExtension($dockerFolders[$i])
     $imageName = "thundernetes-"+$imageName+"-win"
-    docker push $repo$imageName$version
+    docker push $registry$imageName$version
 }
