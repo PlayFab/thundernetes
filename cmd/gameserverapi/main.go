a82efa11557d9d2ae3f9cfe7944360cb1c14bb21
Javier Morales <javiermoralesr95@gmail.com>
2022-05-20 13:45:55 -0400

    Added middleware function with cors policy

--- cmd/gameserverapi/main.go
+++ cmd/gameserverapi/main.go
@@ -58,8 +58,26 @@ func main() {
 	r.Run(addr)
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        c.Writer.Header().Set("Content-Type", "application/json")
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PATCH, PUT, DELETE, UPDATE")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Max")
+        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(200)
+        } else {
+            c.Next()
+        }
+    }
+}
+
 func setupRouter() *gin.Engine {
 	r := gin.Default()
+	r.Use(CORSMiddleware())
 	r.POST(fmt.Sprintf("%s/gameserverbuilds", urlprefix), createGameServerBuild)
 	r.GET(fmt.Sprintf("%s/gameserverbuilds", urlprefix), listGameServeBuilds)
 	r.GET(fmt.Sprintf("%s/gameserverbuilds/:namespace/:buildName", urlprefix), getGameServerBuild)
