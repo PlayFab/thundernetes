2a52c93186e0e04ba860bf2de0fb90215eba9027
Javier Morales <javiermoralesr95@gmail.com>
2022-05-30 14:40:54 -0400

    Removed logger, added specs to GameServerDetail creation

--- cmd/nodeagent/nodeagentmanager.go
+++ cmd/nodeagent/nodeagentmanager.go
@@ -196,9 +196,7 @@ func (n *NodeAgentManager) gameServerCreatedOrUpdated(obj *unstructured.Unstruct
 	if err != nil {
 		logger.Errorf("parsing buildID: %s", err.Error())
 	}
-
-	logger.Infof("parsed buildID: %s", gameServerBuildID)
-
+	
 	// check if the details for this GameServer exist in the map
 	gsdi, exists := n.gameServerMap.Load(gameServerName)
 
@@ -540,12 +538,17 @@ func (n *NodeAgentManager) createGameServerDetails(ctx context.Context, gsuid ty
 		return err
 	}
 
+	spec, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&gsd.Spec)
+	if err != nil {
+		return err
+	}
+
 	u := &unstructured.Unstructured{
 		Object: map[string]interface{}{
 			"apiVersion": gameserverDetailGVR.GroupVersion().String(),
 			"kind":       "GameServerDetail",
 			"metadata":   metadata,
-			"spec":       map[string]interface{}{},
+			"spec":       spec,
 		},
 	}
 
