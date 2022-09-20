a323fee7cd7e0a65090def20ceaa02ab0861de14
mvallillo <mvallillo@microsoft.com>
2022-09-20 17:14:23 -0500

    update plugin syntax

--- cmd/allocator/build-plugin.sh
+++ cmd/allocator/build-plugin.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
-CURR_PATH="$(dirname "$BASH_SOURCE")"
+CURR_PATH="$(cd "$(dirname "$BASH_SOURCE")"; pwd)"
 go build -o $CURR_PATH $CURR_PATH/main.go
-mv $CURR_PATH/command-line-arguments $CURR_PATH/kubectl-gameserver-allocate
-export PATH=$PATH:"$(cd $CURR_PATH; pwd)"
+mv $CURR_PATH/command-line-arguments $CURR_PATH/kubectl-gameserver
+BASH_RC="$(cat ~/.bashrc)"
+if ! [[ $BASH_RC =~ "$CURR_PATH" ]]; then
+  echo "export PATH=\"\$PATH:"$CURR_PATH"\"" >> ~/.bashrc
+fi
 kubectl plugin list
