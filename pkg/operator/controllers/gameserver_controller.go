4560717f7609278e9e94860feae760db81726217
Dimitris-Ilias Gkanatsios <dgkanatsios@outlook.com>
2022-09-20 09:04:01 -0700

    reduce error reporting in GameServer controller (#398)
    * reduce errors in GameServer controller
    
    * updating comment

--- pkg/operator/controllers/gameserver_controller.go
+++ pkg/operator/controllers/gameserver_controller.go
@@ -155,7 +155,11 @@ func (r *GameServerReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 			}
 			// updating GameServer with the new state
 			if err := r.Status().Patch(ctx, &gs, patch); err != nil {
-				return ctrl.Result{}, err
+				// there is a chance that we have arrived here while the GameServer has been deleted
+				// in this case, .Patch will fail with NotFound error and we can safely ignore it
+				if !apierrors.IsNotFound(err) {
+					return ctrl.Result{}, err
+				}
 			}
 			return ctrl.Result{}, nil
 		}
